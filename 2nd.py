4a8945123a5d0892b80ec2beb1fe5a8c2b4f3e0d
cguido95 <153976917+cguido95@users.noreply.github.com>
2023-12-16 12:25:26 -0800

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,35 @@
+
+
+#Lists
+
+mylist = ["banana", "cherry", "apple"]
+print(mylist)
+
+mylist.append("lemon")
+print(mylist)
+
+mylist.insert(0, "blueberry")
+print(mylist)
+
+#item = mylist.pop()
+#print(item)
+#print(mylist)
+
+#item = mylist.remove("cherry")
+#print(item)
+#print(mylist)
+
+#item = mylist.clear()
+#print(item)
+#print(mylist)
+
+#item = mylist.reverse()
+#print(item)
+#print(mylist)
+
+#mylist.sort()
+#new_list = sorted(mylist)
+#print(new_list)
+
+
+
