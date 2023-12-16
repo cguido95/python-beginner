1816c82bb53f0a2c6c2b4b18cd304307a68c2dfb
cguido95 <153976917+cguido95@users.noreply.github.com>
2023-12-16 13:54:09 -0800

    Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -6,67 +6,68 @@ print(mydict)
 mydict2 = {"name" : "Carrie", "age" : 30, "city": "Boston"}
 print(mydict2)
 
-#value = mydict["name"]
-#print(value)
+value = mydict["name"]
+print(value)
 
-#mydict["email"] = "max@xyz.com"
-#print(mydict)
+mydict["email"] = "max@xyz.com"
+print(mydict)
 
-#mydict["email"] = "coolmax@xyz.com"
-#print(mydict)
+mydict["email"] = "coolmax@xyz.com"
+print(mydict)
 
-#del mydict["name"]
-#print(mydict)
+del mydict["name"]
+print(mydict)
 
-#mydict.pop("age")
-#print(mydict)
+mydict.pop("age")
+print(mydict)
 
-#mydict.popitem()
-#print(mydict)
+mydict.popitem()
+print(mydict)
 
-#if "name" in mydict:
-    #print(mydict["name"])
+#if statements
+if "name" in mydict:
+    print(mydict["name"])
 
-#try:
-    #print(mydict["name"])
-#except:
-    #print(mydict["Error"])
+try:
+    print(mydict["name"])
+except:
+    print(mydict["Error"])
 
-#for key in mydict:
-   # print(key)
-
-
-#for key in mydict.keys():
-     #   print(key)
+for key in mydict:
+    print(key)
 
 
-#for key in mydict.values():
- #   print(key)
+for key in mydict.keys():
+    print(key)
 
 
-#for key, value in mydict.items():
-       # print(key, value)
+for key in mydict.values():
+    print(key)
 
 
-#mydict_cpy = mydict
-#print(mydict_cpy)
+for key, value in mydict.items():
+     print(key, value)
 
-#mydict_cpy["email"] = "max@xyz.com"
-#print(mydict_cpy)
-#print(mydict)
+
+mydict_cpy = mydict
+print(mydict_cpy)
+
+mydict_cpy["email"] = "max@xyz.com"
+print(mydict_cpy)
+print(mydict)
 
 
 #combining elements
-#mydict = {"name" : "Max", "age" : 28, "email": "Max@xyz.com"}
-#print(mydict)
+mydict = {"name" : "Max", "age" : 28, "email": "Max@xyz.com"}
+print(mydict)
 
-#mydict2 = {"name" : "Carrie", "age" : 30, "city": "Boston"}
-#print(mydict2)
+mydict2 = {"name" : "Carrie", "age" : 30, "city": "Boston"}
+print(mydict2)
 
-#mydict.update(mydict2)
-#print(mydict)
+mydict.update(mydict2)
+print(mydict)
 
-#mydict = {"name" : "Max", "age" : 28, "email": "Max@xyz.com"}
+mydict = {"name" : "Max", "age" : 28, "email": "Max@xyz.com"}
 
 #dict with numbers
 mydict = {3: 9, 6 : 36, 9: 81}
