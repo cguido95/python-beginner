24f59a914d1e8b0ee0d471a7ce1d15b0f3f16107
cguido95 <153976917+cguido95@users.noreply.github.com>
2023-12-16 12:34:12 -0800

    Lists.py

--- 2nd.py
+++ 2nd.py
@@ -46,4 +46,14 @@ new_list = mylist + mylist2
 print(new_list)
 
 
+#Slices
+mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#a = mylist[1:5]
+#print(a)
+
+#a = mylist[0:]
+#print(a)
+
+#a = mylist[::2]
+#print(a)
 
