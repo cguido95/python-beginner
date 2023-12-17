5dcf839309cb7694bfeaa4c5b32d1e6f571ec160
cguido95 <153976917+cguido95@users.noreply.github.com>
2023-12-17 12:18:55 -0800

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,71 @@
+
+#sets (unordered, mutable, no dupes)
+
+# myset = {1, 2, 3}
+# print(myset)
+#
+# myset2 = set("hello")
+# print(myset2)
+#
+# myset.add(1)
+# myset.add(2)
+# myset.add(3)
+#
+# if 1 in myset:
+#     print("yes")
+# else:
+#     print("No")
+#
+# myset.remove(1)
+#
+# print(myset)
+#
+
+# #intersection, evens, odds, union
+# odds = {1, 3, 5, 7}
+# even = {2, 4, 6, 8}
+# primes = {2, 3, 5, 7}
+#
+# x = odds.union(even)
+# print(x)
+#
+# y = even.intersection(primes)
+# print(y)
+
+#differences
+# x = {1,2,3,4,5,6,7,8,9}
+# y = {1,2,3,10,11,12}
+
+# diff = y.difference(x)
+# print(diff)
+#
+# diff = x.symmetric_difference(y)
+# print(diff)
+#
+# x.update(y)
+# print(x)
+
+# x.intersection_update(y)
+# print(x)
+
+# y.difference_update(x)
+# print(y)
+
+# x.symmetric_difference_update(y)
+# print(x)
+
+#supersets
+# x = {1,2,3,4,5}
+# y = {1,2,3}
+# print(y.issubset(x))
+# print(x.issuperset(y))
+
+# x = {1,2,3,4,5}
+# y = x
+# y.add(6)
+# print(y)
+# print(x)
+
+
+
+
