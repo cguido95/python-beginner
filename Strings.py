5640494074511a21933e47e7da4700525058c940
cguido95 <153976917+cguido95@users.noreply.github.com>
2023-12-17 16:59:37 -0800

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -1,81 +1,81 @@
-# # #Strings = imutable (cannot be changed)
-#
-# # # mystring = "Hello World"
-# # # print(mystring)
-# # #
-# # # mystring = """Hello
-# # # World"""""
-# # # print(mystring)
-# # #
-# # # mystring = """Hello \
-# # # World"""""
-# # # print(mystring)
+# #Strings = imutable (cannot be changed)
+
+# # mystring = "Hello World"
+# # print(mystring)
 # #
-# # my_string = "Hello World"
-# # char = my_string[0]
-# # print(char)
+# # mystring = """Hello
+# # World"""""
+# # print(mystring)
 # #
-# # my_string = "Hello World"
-# # char = my_string[0]
-# # print(char)
-# #
-# # my_string = "Hello World"
-# # substring = my_string[::-1]
-# # print(substring)
-# #
-# # my_string = "Hello World"
-# # substring = my_string[::1]
-# # print(substring)
-# #
-# # greeting = "Hello World"
-# # name = "Cesar"
-# # sentence = greeting + " " + name
-# # print(sentence)
+# # mystring = """Hello \
+# # World"""""
+# # print(mystring)
 #
-# #replace,find,count strings
-# # my_string = "Hello World"
-# # print(my_string.replace("Hello", "Pussy"))
+# my_string = "Hello World"
+# char = my_string[0]
+# print(char)
 #
+# my_string = "Hello World"
+# char = my_string[0]
+# print(char)
 #
-# # #String splitting
-# # my_string = "How are you doing"
-# # my_list = my_string.split()
-# # print(my_list)
-# #
-# # my_string = "How are you doing"
-# # my_list = my_string.split(",")
-# # new_string = "".join(my_list)
-# # print(new_string)
+# my_string = "Hello World"
+# substring = my_string[::-1]
+# print(substring)
 #
+# my_string = "Hello World"
+# substring = my_string[::1]
+# print(substring)
 #
-# #timer string
-# # from timeit import default_timer as timer
-# # my_list = ["a"] * 1000000
-# # #good
-# # start = timer()
-# # my_string = "".join(my_list)
-# # stop = timer()
-# # print(stop - start)
+# greeting = "Hello World"
+# name = "Cesar"
+# sentence = greeting + " " + name
+# print(sentence)
+
+#replace,find,count strings
+# my_string = "Hello World"
+# print(my_string.replace("Hello", "Pussy"))
+
+
+# #String splitting
+# my_string = "How are you doing"
+# my_list = my_string.split()
+# print(my_list)
 #
-#
-#
-# #variables
-# var = "Cesar"
-# my_string = "the variable is %s" % var
-# print(my_string)
-#
-# #decimal value = d
-# var = 5.9
-# my_string = "the variable is %d" % var
-# print(my_string)
-#
-# #f = floating value
-# var = 5.9
-# my_string = "the variable is %f" % var
-# print(my_string)
-#
-# # number of digits left
-# var = 5.9
-# my_string = "the variable is %.4f" % var
-# print(my_string)
-# 
+# my_string = "How are you doing"
+# my_list = my_string.split(",")
+# new_string = "".join(my_list)
+# print(new_string)
+
+
+#timer string
+# from timeit import default_timer as timer
+# my_list = ["a"] * 1000000
+# #good
+# start = timer()
+# my_string = "".join(my_list)
+# stop = timer()
+# print(stop - start)
+
+
+
+#variables
+var = "Cesar"
+my_string = "the variable is %s" % var
+print(my_string)
+
+#decimal value = d
+var = 5.9
+my_string = "the variable is %d" % var
+print(my_string)
+
+#f = floating value
+var = 5.9
+my_string = "the variable is %f" % var
+print(my_string)
+
+# number of digits left
+var = 5.9
+my_string = "the variable is %.4f" % var
+print(my_string)
+
