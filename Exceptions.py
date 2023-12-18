90c7c9d18a4ccd5e42eeb6b5eb6b89d30957aafc
cguido95 <153976917+cguido95@users.noreply.github.com>
2023-12-17 23:33:01 -0800

    Add files via upload

---
+++ Exceptions.py
@@ -0,0 +1,33 @@
+#Exceptions
+
+# x = -5
+# if x < 0:
+#     raise Exception("x should be positive")
+
+#assert
+# x = -5
+# assert(x >= 0), "x is not positive"
+
+# try:
+#     a = 5/0
+# except:
+#     print("an error happened")
+#
+# try:
+#     a = 5/0
+# except Exception as e:
+#     print(e)
+
+
+# try:
+#     a = 5/1
+#     B = a + 4
+# except ZeroDivisionError as e:
+#     print(e)
+# except TypeError as e:
+#     print(e)
+# else:
+#     print("Everything is ok")
+# finally:
+#     print("cleaning up...")
+
