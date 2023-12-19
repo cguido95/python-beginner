a4fdee4f77bf9b95395fb3c20aaeebb5c7845e92
cguido95 <153976917+cguido95@users.noreply.github.com>
2023-12-18 23:22:03 -0800

    Add files via upload

---
+++ Random Numbers.py
@@ -0,0 +1,58 @@
+#Random Numbers
+
+import random
+import secrets
+
+# a = random.random()
+# print(a)
+#
+# a = random.uniform(1, 10)
+# print(a)
+#
+# a = random.randint(1, 10)
+# print(a)
+#
+# a = random.randrange(1, 10)
+# print(a)
+#
+# a = random.normalvariate(1, 10)
+# print(a)
+
+# mylist = list("ABCDEFGH")
+# a = random.choice(mylist)
+# print(a)
+#
+# mylist = list("ABCDEFGH")
+# a = random.sample(mylist, 3)
+# print(a)
+#
+# mylist = list("ABCDEFGH")
+# a = random.choices(mylist, k=3)
+# print(a)
+#
+# mylist = list("ABCDEFGH")
+# a = random.shuffle(mylist)
+# print(mylist)
+
+
+#random seed
+# random.seed(1)
+# print(random.random())
+# print(random.randint(1,10))
+# random.seed(2)
+# print(random.random())
+# print(random.randint(1,10))
+#
+# random.seed(1)
+# print(random.random())
+# print(random.randint(1,10))
+# random.seed(2)
+# print(random.random())
+# print(random.randint(1,10))
+
+# a = secrets.randbits(4)
+# print(a)
+
+
+
+
